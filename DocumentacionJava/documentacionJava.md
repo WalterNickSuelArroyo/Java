4684be1703b89f2a38c483672235f39705730ae6
WALTERSUEL <nick_pq_0@hotmail.com>
2022-07-10 20:54:48 -0500

    173. Panel (JPanel) - Creacion de un panel

--- DocumentacionJava/documentacionJava.md
+++ DocumentacionJava/documentacionJava.md
@@ -661,3 +661,27 @@ public class Ventana extends JFrame{
     }
 }
 ```
+# 173. Panel (JPanel) - Creacion de un panel
+```java
+package ventana;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Ventana extends JFrame{
+    public Ventana(){
+        setSize(500, 500);
+        setTitle("El mejor titulo"); 
+        setLocationRelativeTo(null);
+        setMinimumSize(new Dimension(200,200)); 
+        iniciarComponentes();
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private void iniciarComponentes(){
+        JPanel panel = new JPanel(); //Creacion de un panel
+        this.getContentPane().add(panel);//Agregamos el panel a la ventana
+        panel.setBackground(Color.GREEN); //Establecemos el color del panel
+    }
+}
+```
--- JavaATS/Seccion 13/Ventana/src/ventana/Ventana.java
+++ JavaATS/Seccion 13/Ventana/src/ventana/Ventana.java
@@ -2,19 +2,21 @@ package ventana;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class Ventana extends JFrame{
     public Ventana(){
-        setSize(500, 500); //Establecemos el tamaño de la ventana
-        setTitle("El mejor titulo"); //Establecemos el titulo de la ventana
-        //setLocation(100, 200); // Establecemos la posicion inicial de la ventana
-        //setBounds(100, 200, 500, 500); //Juntamos el setlocation con setsize
-        setLocationRelativeTo(null); //Establecemos la ventana en el centro
-        
-        //setResizable(false); //Establecemos si la ventana puede cambiar de tamaño o no
-        setMinimumSize(new Dimension(200,200)); //Establecemos el tamaño minimo
-        
-        this.getContentPane().setBackground(Color.blue);
+        setSize(500, 500);
+        setTitle("El mejor titulo"); 
+        setLocationRelativeTo(null);
+        setMinimumSize(new Dimension(200,200)); 
+        iniciarComponentes();
         
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+    private void iniciarComponentes(){
+        JPanel panel = new JPanel(); //Creacion de un panel
+        this.getContentPane().add(panel);//Agregamos el panel a la ventana
+        panel.setBackground(Color.GREEN); //Establecemos el color del panel
+    }
+    
 }

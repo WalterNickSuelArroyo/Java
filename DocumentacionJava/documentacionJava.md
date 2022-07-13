a1ce47df1f6b9e63537fbc6bd6914b27f2f090da
WALTERSUEL <nick_pq_0@hotmail.com>
2022-07-13 00:37:31 -0500

    185. Crear botonoes de activacion (JToggleButton)

--- DocumentacionJava/documentacionJava.md
+++ DocumentacionJava/documentacionJava.md
@@ -1296,3 +1296,62 @@ public class Ventana extends JFrame{
     }
 }
 ```
+# 185. Crear botonoes de activacion (JToggleButton)
+```java
+package ventana;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
+public class Ventana extends JFrame{
+    public JPanel panel;
+    
+    public Ventana(){
+        setSize(900, 900);
+        setTitle("El mejor titulo"); 
+        setLocationRelativeTo(null);
+        setMinimumSize(new Dimension(200,200)); 
+        iniciarComponentes();
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private void iniciarComponentes(){
+        colocarPaneles();
+        colocarBotonesDeActivacion();
+    }
+    private void colocarPaneles(){
+        panel = new JPanel(); //Creacion de un panel
+        panel.setLayout(null); //Desactivamos el diseño por defecto
+        this.getContentPane().add(panel);//Agregamos el panel a la ventana
+    }
+    
+    private void colocarBotonesDeActivacion(){
+        JToggleButton botonActivacion1 =  new JToggleButton("Opcion 1",true);
+        botonActivacion1.setBounds(50, 100, 100, 40);
+        panel.add(botonActivacion1);
+        
+        JToggleButton botonActivacion2 =  new JToggleButton("Opcion 2",false);
+        botonActivacion2.setBounds(50, 150, 100, 40);
+        panel.add(botonActivacion2);
+        
+        JToggleButton botonActivacion3 =  new JToggleButton("Opcion 3",false);
+        botonActivacion3.setBounds(50, 200, 100, 40);
+        panel.add(botonActivacion3);
+        
+        ButtonGroup grupoBotonesActivados = new ButtonGroup();
+        grupoBotonesActivados.add(botonActivacion1);
+        grupoBotonesActivados.add(botonActivacion2);
+        grupoBotonesActivados.add(botonActivacion3);
+    }
+}
+```
--- JavaATS/Seccion 13/Ventana/src/ventana/Ventana.java
+++ JavaATS/Seccion 13/Ventana/src/ventana/Ventana.java
@@ -11,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 public class Ventana extends JFrame{
     public JPanel panel;
@@ -26,78 +27,30 @@ public class Ventana extends JFrame{
     }
     private void iniciarComponentes(){
         colocarPaneles();
-        //colocarEtiquetas();
-        //colocarBotones();
-        colocarRadioBotones();
+        colocarBotonesDeActivacion();
     }
     private void colocarPaneles(){
         panel = new JPanel(); //Creacion de un panel
         panel.setLayout(null); //Desactivamos el diseño por defecto
         this.getContentPane().add(panel);//Agregamos el panel a la ventana
     }
-    private void colocarEtiquetas(){
-        JLabel etiqueta = new JLabel(); //Creamos una etiqueta
-        etiqueta.setText("Mundial 2018"); //Establecemos el texto de la etiqueta
-        etiqueta.setHorizontalAlignment(SwingConstants.CENTER);//Establecemos la alineacion horizontal del texto
-        etiqueta.setBounds(230, 10, 300, 80);
-        etiqueta.setForeground(Color.BLACK);//Establecemos el color de la letra
-        etiqueta.setFont(new Font("cooper black",0,40));
-        panel.add(etiqueta); //Agregamos la etiqueta al panel
-        
-        //Etiqueta 2 - etiqueta tipo imagen
-        ImageIcon imagen = new ImageIcon("balon.jpg");
-        JLabel etiqueta2 = new JLabel();
-        etiqueta2.setBounds(10, 80, 750, 555);
-        etiqueta2.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(etiqueta2.getWidth(),etiqueta2.getHeight(),Image.SCALE_SMOOTH)));
-        panel.add(etiqueta2);
-    }
     
-    private void colocarBotones(){
-        //Boton 1 - boton de texto
-        JButton boton1 = new JButton();
-        boton1.setText("Click");//Establecemos un texto al boton
-        boton1.setBounds(100, 100, 100, 40);
-        boton1.setEnabled(true);//Habilita o desabilita un boton
-        boton1.setMnemonic('b');//Establecemeos alt+letra
-        boton1.setForeground(Color.BLUE);
-        boton1.setFont(new Font("cooper black", 3, 20));
-        panel.add(boton1);
+    private void colocarBotonesDeActivacion(){
+        JToggleButton botonActivacion1 =  new JToggleButton("Opcion 1",true);
+        botonActivacion1.setBounds(50, 100, 100, 40);
+        panel.add(botonActivacion1);
         
-        //Boton 2 - boton de imagen
-        JButton boton2 = new JButton();
-        boton2.setBounds(100, 200, 100, 40);
-        //boton2.setOpaque(true);
-        ImageIcon imagen2 = new ImageIcon("check.png");
-        boton2.setIcon(new ImageIcon(imagen2.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(), Image.SCALE_SMOOTH)));
-        //boton2.setBackground(Color.blue);
-        panel.add(boton2);
+        JToggleButton botonActivacion2 =  new JToggleButton("Opcion 2",false);
+        botonActivacion2.setBounds(50, 150, 100, 40);
+        panel.add(botonActivacion2);
         
-        //Boton 3 - boton de bordes
-        JButton boton3 = new JButton();
-        boton3.setBounds(100, 300, 110, 50);
-        boton3.setBorder(BorderFactory.createLineBorder(Color.blue, 4, true));
-        panel.add(boton3);
-    }
-    
-    private void colocarRadioBotones(){
-        JRadioButton radioBoton1 = new JRadioButton("Opcion 1", true);
-        radioBoton1.setBounds(50, 100, 200, 50);
-        radioBoton1.setEnabled(true);
-        radioBoton1.setText("Programacion");
-        radioBoton1.setFont(new Font("cooper black",0,20));
-        panel.add(radioBoton1);
+        JToggleButton botonActivacion3 =  new JToggleButton("Opcion 3",false);
+        botonActivacion3.setBounds(50, 200, 100, 40);
+        panel.add(botonActivacion3);
         
-        JRadioButton radioBoton2 = new JRadioButton("Opcion 2", false);
-        radioBoton2.setBounds(50, 150, 100, 50);
-        panel.add(radioBoton2);
-        
-        JRadioButton radioBoton3 = new JRadioButton("Opcion 3", false);
-        radioBoton3.setBounds(50, 200, 100, 50);
-        panel.add(radioBoton3);
-        
-        ButtonGroup grupoRadioBotones = new ButtonGroup();
-        grupoRadioBotones.add(radioBoton1);
-        grupoRadioBotones.add(radioBoton2);
-        grupoRadioBotones.add(radioBoton3);
+        ButtonGroup grupoBotonesActivados = new ButtonGroup();
+        grupoBotonesActivados.add(botonActivacion1);
+        grupoBotonesActivados.add(botonActivacion2);
+        grupoBotonesActivados.add(botonActivacion3);
     }
 }
